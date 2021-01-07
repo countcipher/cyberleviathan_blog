b90cd58f0d2b7b72f79f3be814a4bd9f52234f50
Count Cipher <chrisharlow366@yahoo.com>
2021-01-07 12:39:35 -0500

    com

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'biz_wordpress' );
+//define( 'DB_NAME', 'biz_wordpress' );
+define( 'DB_NAME', 'chriirfa_biz_wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+//define( 'DB_USER', 'root' );
+define( 'DB_USER', 'chriirfa_g0l1ath' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+//define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'dv=M@(^hCLkv' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
