16496eabcecda0227f20ad0ab8ef705c2b7a7e49
Count Cipher <chrisharlow366@yahoo.com>
2021-01-08 12:38:05 -0500

    com

--- wp-content/themes/cyber_leviathan/header.php
+++ wp-content/themes/cyber_leviathan/header.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Empower YOUR business online with Cyber Leviathan's custom websites and online marketing" />
        
     <title>Cyber Leviathan Blog</title>
 
